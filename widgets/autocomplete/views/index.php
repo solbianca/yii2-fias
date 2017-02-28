35e8bc4af7c40e4e911f86dacac5e6889cc85fa1
Evgeniy P <ponkratenko@gmail.com>
2017-02-28 14:06:59 +0300

    getFullNumber

--- widgets/autocomplete/views/index.php
+++ widgets/autocomplete/views/index.php
@@ -25,7 +25,7 @@
         $region = $address['city']->region_code;
         $city = [['id' => $address['city']->address_id, 'value' => $address['city']->getFullAddress(), 'mark' => 1]];
         $street = [['id' => $address['street']->address_id, 'value' => $address['street']->getFullAddress(), 'mark' => 1]];
-        $house = [['id' => $address['house']->house_id, 'value' => $address['house']->number, 'mark' => 1]];
+        $house = [['id' => $address['house']->house_id, 'value' => $address['house']->getFullNumber(), 'mark' => 1]];
     }
 ?>
 
